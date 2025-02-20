02d05b49947fefff490f091184ed5253ce96a120
imb-bestrada <bstrada4@gmail.com>
2025-02-20 18:56:10 -0500

    UPD TEST

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
     
     await app.listen(envs.port);
 
-    console.log("Hola Mundo- Primer cambio!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    console.log("Hola Mundo- Primer cambio!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 
     logger.log(`Gateway running on port ${ envs.port }`);
 
